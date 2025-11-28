5c72202ce06b77cb5652a03589f85e25b7485e4a
Junaid <158271661+junaid-h0ssain@users.noreply.github.com>
2025-11-28 19:09:58 +0600

    build issue fix

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,21 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   root: './',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        welcome: resolve(__dirname, 'welcome.html'),
+        app: resolve(__dirname, 'app.html'),
+        about: resolve(__dirname, 'public/about.html'),
+        farmer_stories: resolve(__dirname, 'public/farmer_stories.html'),
+        our_research: resolve(__dirname, 'public/our_research.html'),
+      },
+    },
   },
   server: {
     port: 5173,
